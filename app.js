7f7a3d993441da8c9aa96c3a668b6f49327026c3
12345rams <vijay90054442298@gmail.com>
2024-01-26 19:35:03 +0530

    fixed

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({limit:'50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 const storage = multer.diskStorage({
