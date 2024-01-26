97b833bb2367fc226cb686dd2e310554a0043d1f
12345rams <vijay90054442298@gmail.com>
2024-01-26 22:44:49 +0530

    Bug Fixed

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+router.use(cookieParser());
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  // Check if the 'token' cookie is present in the request
+  const token = req.cookies.token;
 
+  if (token) {
+    // If the token is present, verify it and extract information
+    jwt.verify(token, process.env.TOKENCODE, (err, decoded) => {
+      if (err) {
+        // Token is invalid or expired
+        res.render('index', { isLoggedIn: false });
+      } else {
+        // Token is valid, you can use decoded data
+        res.render('index', { isLoggedIn: true });
+      }
+    });
+  } else {
+    // Token is not present
+    res.render('index', { isLoggedIn: false });
+  }
+});
 
 module.exports = router;
