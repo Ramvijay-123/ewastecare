05c644ff21d5c1383818961a1a2e0e8d9f4707c4
Ram Vijay Yadav <132057037+12345rams@users.noreply.github.com>
2024-07-07 13:21:10 +0530

    Update connectDb.js

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 const connectDb=()=>{
-    mongoose.connect("mongodb+srv://bitbytecoders:bitbytecoder123@cluster0.shit8by.mongodb.net/ewaste").then((data,err)=>{
+    mongoose.connect("").then((data,err)=>{
  
     if(err) console.log(err);
     else    console.log(`Mongodb  is connected ${data.connection.port}`);
