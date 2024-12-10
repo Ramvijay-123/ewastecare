eb27b6516c67d4620b9e7107a45d0d7a4c578c15
Ramvijay-123 <132054544+Ramvijay-123@users.noreply.github.com>
2024-12-11 01:19:14 +0530

    Update cart.js

--- routes/cart.js
+++ routes/cart.js
@@ -1,21 +1,27 @@
-// Get modal elements
+
 var express = require('express');
 var cart =require("../model/cartModel")
 var router = express.Router();
 
-/* GET users listing. */
 router.post('/', async function(req, res, next) {
-    const {token} =req.cookies
-    const data= await cart.create({
-        nameOfProduct:req.body.nameOfProduct,
-         price:req.body.price,
-         img:req.body.img,
-         id:token,
-         discription:req.body.description,
-       })
+  const { token } = req.cookies;
+  if (!token) {
+      return res.status(401).send('You need to log in to add items to your cart');
+  }
 
-res.send('Cart added successfuly');
-  
+  try {
+      const data = await cart.create({
+          nameOfProduct: req.body.nameOfProduct,
+          price: req.body.price,
+          img: req.body.img,
+          id: token,  
+          discription: req.body.description,
+      });
+      res.send('Cart added successfully');
+  } catch (error) {
+      res.status(500).send('Something went wrong');
+  }
 });
+
 
 module.exports = router;
